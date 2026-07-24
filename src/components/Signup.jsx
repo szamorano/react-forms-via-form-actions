9ed4cbb30704891c76e4f1f84c4ba18f9ac9edf8
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-24 18:51:48 -0400

    Added validation checks.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,49 @@
+import {
+  isEmail,
+  isNotEmpty,
+  isEqualToOtherValue,
+  hasMinLength,
+} from "../util/validation";
+
 export default function Signup() {
   function signupAction(formData) {
-    const enteredEmail = formData.get("email");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+    const firstName = formData.get("first-name");
+    const lastName = formData.get("last-name");
+    const role = formData.get("role");
+    const terms = formData.get("terms");
+    const acquisitionChannel = formData.getAll("acquisition");
     console.log(enteredEmail);
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push("Invalid email address.");
+    }
+
+    if (!isEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least six characters.");
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords do not match.");
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("Provide both first and last name.");
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push("Select a role.");
+    }
+    if (!terms) {
+      errors.push("You must agree to the terms.");
+    }
+    if (acquisitionChannel.length === 0) {
+      errors.push("please select at least one acquisition channel.");
+    }
   }
 
   return (
